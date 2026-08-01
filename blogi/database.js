8f13968b19d6f8914be0379dbff154629e225653
Rasmus <hautamakiras@gmail.com>
2026-08-01 20:34:51 +0300

    added blog

---
+++ blogi/database.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://mwisnfhkwqscitufragh.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_nrWi8Co1-Eg77yM3-JsC6g__aOK3nqM';
+
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
